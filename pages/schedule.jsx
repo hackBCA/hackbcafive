f58fb00cb8ce1529988730bac2b42870088f67a1
Anthony Li <li.anthony917@gmail.com>
2020-02-04 17:26:26 -0500

    Add More buttons/details

--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import { Row, Col } from "react-bootstrap";
 import { DateTime } from "luxon";
 import ReactMarkdown from "react-markdown";
+import ChevronRight from "../components/ChevronRight";
 
 function getTime(date) {
     return DateTime.fromJSDate(date).setZone("America/New_York").toFormat("HH:mm");
@@ -31,9 +32,33 @@ export default () => {
             left: 0;
             right: 0;
             width: 100%;
-            height: 96px;
+            height: 128px;
             pointer-events: none;
             z-index: 1;
+        }
+
+        .hackbca-event-type {
+            letter-spacing: 0.1rem;
+            font-weight: 600;
+            font-size: 0.8rem;
+        }
+
+        .hackbca-event-time {
+            white-space: nowrap;
+        }
+
+        .hackbca-event-more-link {
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            z-index: 2;
+        }
+
+        .hackbca-event-details {
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            z-index: 2;
         }
         `}</style>
         <h1>Schedule</h1>
@@ -47,12 +72,18 @@ export default () => {
                     key={index}
                 >
                     <div className="rounded shadow hackbca-event p-3 w-100 h-100">
+                        {isHalfWidth && <div className="hackbca-event-type text-uppercase text-white-50">{event.type}</div>}
                         {isHalfWidth ? <div className="d-flex justify-content-between align-items-center w-100">
                             <h4 className="font-weight-bold w-auto flex-shrink-1">{event.name}</h4>
-                            <span className="h5 pl-3">{getTime(event.start)}{event.end && <span className="text-white-50"> - {getTime(event.end)}</span>}</span>
+                            <span className="h5 pl-3 hackbca-event-time">{getTime(event.start)}{event.end && <span className="text-white-50"> - {getTime(event.end)}</span>}</span>
                         </div> : <span><strong>{getTime(event.start)}{event.end && `-${getTime(event.end)}`}</strong> - {event.name}</span>}
                         {isHalfWidth && (event.description ? <ReactMarkdown source={event.description.replace(/\n/g, "\n\n")} linkTarget="_blank" /> : <p className="text-white-50"><em>No description available.</em></p>)}
-                        {isHalfWidth && <div className="hackbca-event-scrim rounded-bottom"></div>}
+                        {isHalfWidth && (event.description || event.presenter || event.requirements) && <div className="hackbca-event-scrim rounded-bottom"></div>}
+                        {isHalfWidth && (event.presenter || event.requirements) && <div className="hackbca-event-details pl-3 pb-3">
+                            {event.presenter && <div>{event.presenter}</div>}
+                            {event.requirements && <div className="text-white-50">This workshop has special requirements.</div>}
+                        </div>}
+                        {(event.description || event.requirements) && <div className="hackbca-event-more-link pr-3 pb-3"><a href="#">More <ChevronRight /></a></div>}
                     </div>
                 </Col>
             })}
