4aee4f1ea17532bca9573e6dab5a2d9e53a1914c
Anthony Li <li.anthony917@gmail.com>
2020-02-13 17:19:56 -0500

    Add iOS stuff

---
+++ pages/ios.jsx
@@ -0,0 +1,32 @@
+import ContentPage from "../components/ContentPage";
+
+export default () => <ContentPage title="iOS Workshop">
+    <h1>iOS Workshop</h1>
+
+    <h2>Downloads</h2>
+    <ul>
+        <li><a href="https://apps.apple.com/us/app/xcode/id497799835?mt=12" target="_blank"><strong>Xcode - please download!</strong></a></li>
+        <li><a href="https://github.com/hackBCA/hackBCAV-iOS/blob/master/duck-clicker/Duck%20Clicker/Assets.xcassets/Duck.imageset/rubber_duck_PNG39.png" target="_blank">Duck image</a></li>
+        <li><a href="https://raw.githubusercontent.com/hackBCA/hackBCAV-iOS/master/duck-clicker/Duck%20Clicker/Duck.m4a" target="_blank">Duck sound</a></li>
+    </ul>
+
+    <h2>Resources</h2>
+    
+    <h3>By Apple</h3>
+    <ul>
+        <li><a href="https://docs.swift.org/swift-book/GuidedTour/GuidedTour.html"><em>A Swift Tour</em> - Tour of the Swift programming language</a></li>
+        <li><a href="https://developer.apple.com/tutorials/SwiftUI"><em>SwiftUI Tutorials</em> - Helpful tutorials by Apple</a></li>
+        <li><a href="https://developer.apple.com/videos/wwdc2019">WWDC Videos - Videos by Apple about macOS and iOS development</a></li>
+    </ul>
+
+    <h3>By Others</h3>
+    <ul>
+        <li><a href="https://www.hackingwithswift.com/articles/196/learn-swiftui-with-free-tutorials">Hacking with Swift's collection of SwiftUI tutorials</a></li>
+    </ul>
+
+    <h3>Integrating UIKit</h3>
+    <ul>
+        <li><a href="https://developer.apple.com/tutorials/swiftui/interfacing-with-uikit">Interfacing SwiftUI with UIKit - a tutorial by Apple</a></li>
+        <li><a href="https://code.tutsplus.com/tutorials/ios-from-scratch-with-swift-first-steps-with-uikit--cms-25461">Intro to UIKit</a></li>
+    </ul>
+</ContentPage>
